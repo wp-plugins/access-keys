17ce523c3e95b9fe9b04dd02dde6d3aec0307737
Aaron Harun <Aaron Harun@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-06 01:46:42 +0000

    Version 1.0 released.
    git-svn-id: https://plugins.svn.wordpress.org/access-keys/trunk@38551 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- access_keys.php
+++ access_keys.php
@@ -4,7 +4,7 @@ Plugin Name: Access Keys
 Plugin URI: http://anthologyoi.com/wordpress/plugins/wordpress-access-keys.html
 Description: This plugin allows you to add Access Keys to Category and Page navigation menus to make your website far more accessible.
 Author: Aaron Harun
-Version: 0.6
+Version: 1.0
 Author URI: http://anthologyoi.com/
 */
 
@@ -13,142 +13,63 @@ Installation:
 ONE: Upload to your plugin directory.
 TWO: Activate.
 THREE: Go to the Admin Menu under Manage named Access Keys.
-FOUR: Read the instructions on the Admin Panel. 
+FOUR: Read the instructions on the Admin Panel.
 
 */
  $access_keys = get_option('access_keys');
+
 // *******************************
 // Add Category Access Keys
 // *******************************
+add_filter('wp_list_categories', 'access_keys_cats');
 
-if(is_home() || is_archive() || is_singular()){
-	add_filter('wp_list_categories', 'access_keys_fix_links');
-	
-	if (!strpos($_SERVER['PHP_SELF'], 'wp-admin')){
-		if (version_compare($wp_version, '2.3', '<')){
-			add_filter('get_categories', 'access_keys_categories_add_key_wp21');
-		}else{
-			add_filter('get_terms', 'access_keys_categories_add_key',10,2);
-		}
-		
-	}
+function access_keys_cats($cats) {
+		return preg_replace_callback('!(<li class="cat-item (cat)-item-([0-9]*)">[\s\S]*?<a([^>]*)>)!ims', 'access_keys_finish', $cats);
 }
-
-function access_keys_categories_add_key_wp21($cats){
-global $access_keys;
-$cats_temp=array();
-	foreach ($cats as $cat){
-		if(strlen($access_keys['cat'][$cat->cat_ID])>0){
-			$cat->cat_name .= 'accesskey=@'.$access_keys['cat'][$cat->cat_ID].'@';
-		}
-		$cats_temp[]=$page;
-	}
-
-$cats=$cats_temp; 
-return $cats;
-
-}
-
-function access_keys_categories_add_key($cats,$is_cat){
-global $access_keys;
-	if($is_cat[0] == 'category'){
-		$cats_temp=array();
-		foreach ($cats as $cat){
-			if(strlen($access_keys['cat'][$cat->term_id])>0){
-				$cat->name .= 'accesskey=@'.$access_keys['cat'][$cat->term_id].'@';
-				
-			}
-			$cats_temp[]=$cat;
-		}
-	
-		$cats=$cats_temp; 
-	}
-return $cats;
-
-}
-
 
 // *******************************
 // Add Page Access Keys
 // *******************************
-add_filter('wp_list_pages', 'access_keys_fix_links');
-if (!strpos($_SERVER['PHP_SELF'], 'wp-admin')){
-	if (version_compare($wp_version, '2.3', '<')){
-		add_filter('get_pages', 'access_keys_pages_add_key21');
-	}else{
-		add_filter('get_pages', 'access_keys_pages_add_key');
-	}
-	
+add_filter('wp_list_pages', 'access_keys_pages');
+
+function access_keys_pages($pages) {
+	return preg_replace_callback('!(<li class="page_item (page)-item-([0-9]*)"><a([^>]*)>)!ims', 'access_keys_finish', $pages);
 }
-function access_keys_pages_add_keywp21($pages){
-global $access_keys,$id;
-$pages_temp=array();
-if(!$id){
-	foreach ($pages as $page){
-		if(strlen($access_keys['page'][$page->ID])>0){
-			$page->post_title .= 'accesskey=@'.$access_keys['page'][$page->ID].'@';
-		}
-		$pages_temp[]=$page;
-	}
-$pages=$pages_temp; 
-}
-	
-return $pages;
-
-}
-
-function access_keys_pages_add_key($pages){
-global $access_keys,$id;
-$pages_temp=array();
-
-	foreach ($pages as $page){
-		if(strlen($access_keys['page'][$page->ID])>0){
-			$page->post_title .= 'accesskey=@'.$access_keys['page'][$page->ID].'@';
-		}
-		$pages_temp[]=$page;
-	}
-$pages=$pages_temp; 
-
-	
-return $pages;
-
-}
-
 
 // *******************************
 // Add Access Keys
 // *******************************
 
-function access_keys_fix_links($cats) {
-	return preg_replace_callback('!<a[^>]*>[^>]*?(accesskey=@([^<]*)@)</a>!ims', 'access_keys_finish', $cats);
-}
 
 function access_keys_finish($matches){
-	if($matches[1])
-		$link = preg_replace('/accesskey=\@.*?\@/','',$matches[0]);
-	
-	if(!is_int($matches[2]) || $matches[2]>=0)
-		$link = str_replace(' title=', ' accesskey="'.$matches[2].'" title=', $link);
-	
+global $access_keys;
+	$id = $matches[3];
+	$link = $matches[0];
+	if($access_keys[$matches[2]][$id]){
+		$accesskey = $matches[4]. ' accesskey="'.$access_keys[$matches[2]][$id].'" ';
+		$link = str_replace($matches[4], $accesskey, $link);
+	}
 return $link;
 }
+
 
 // *******************************
 // Admin Panel
 // *******************************
-	function access_keys_update_options($options){
-	global $access_keys;
-		while (list($option, $value) = each($options)) {
-				$access_keys[$option] =$value;
-		}
-	return $access_keys;
+function access_keys_update_options($options){
+global $access_keys;
+	while (list($option, $value) = each($options)) {
+			$access_keys[$option] =$value;
 	}
+return $access_keys;
+}
 
 
 add_action('admin_menu', 'access_keys_menu');
-	function access_keys_menu() {
-		add_submenu_page('edit.php', 'Access Keys', 'Access Keys', 8, __FILE__,'access_keys_admin');
-	}
+
+function access_keys_menu() {
+	add_submenu_page('edit.php', 'Access Keys', 'Access Keys', 8, __FILE__,'access_keys_admin');
+}
 
 function access_keys_admin(){
 global $access_keys;
@@ -184,35 +105,6 @@ $catsnokey = array();
 		}
 	}
 ?>
-
-
-<script type="text/javascript" src="../wp-includes/js/dbx.js"></script>
-<script type="text/javascript" src="../wp-includes/js/tw-sack.js"></script>
-<script type="text/javascript">
-				//<![CDATA[
-				addLoadEvent( function() {
-					var manager = new dbxManager('inap');
-					
-					//create new docking boxes group
-					var advanced = new dbxGroup(
-						'advancedstuff', 		// container ID [/-_a-zA-Z0-9/]
-						'vertical', 		// orientation ['vertical'|'horizontal']
-						'10', 			// drag threshold ['n' pixels]
-						'yes',			// restrict drag movement to container axis ['yes'|'no']
-						'0', 			// animate re-ordering [frames per transition, or '0' for no effect]
-						'yes', 			// include open/close toggle buttons ['yes'|'no']
-						'open', 		// default state ['open'|'closed']
-						'open', 		// word for "open", as in "open this box"
-						'close', 		// word for "close", as in "close this box"
-						'click-down and drag to move this box', // sentence for "move this box" by mouse
-						'click to %toggle% this box', // pattern-match sentence for "(open|close) this box" by mouse
-						'use the arrow keys to move this box', // sentence for "move this box" by keyboard
-						', or press the enter key to %toggle% it',  // pattern-match sentence-fragment for "(open|close) this box" by keyboard
-						'%mytitle%  [%dbxtitle%]' // pattern-match syntax for title-attribute conflicts
-						);
-				});
-				//]]>
-</script>
 <div class="wrap">
 <form method="post">
 <h2>Access Keys Configuration</h2>
@@ -229,7 +121,7 @@ $catsnokey = array();
 			</div>
 		</div>
 	</fieldset>
-</div>				
+</div>
 <div class="dbx-b-ox-wrapper">
 	<fieldset id="accesskeys" class="dbx-box">
 		<div class="dbx-h-andle-wrapper"><h3 class="dbx-handle">Current Access Keys</h3></div>
@@ -239,7 +131,7 @@ $catsnokey = array();
 <?php foreach($pagesyeskey as $x){
 echo <<<block
     <li><p>
-					<label>Access Key for $x[1] <input type="text" maxlength="1" value="$x[2]" name="access_keys[page][$x[0]]"></label>
+		<label>Access Key for $x[1] <input type="text" maxlength="1" value="$x[2]" name="access_keys[page][$x[0]]"></label>
     </p></li>
 block;
 }?>
@@ -251,7 +143,7 @@ Categories:
 <?php foreach($catsyeskey as $x){
 echo <<<block
     <li><p>
-					<label>Access Key for $x[1] <input type="text" maxlength="1" value="$x[2]" name="access_keys[cat][$x[0]]"></label>
+		<label>Access Key for $x[1] <input type="text" maxlength="1" value="$x[2]" name="access_keys[cat][$x[0]]"></label>
     </p></li>
 block;
 }?>
--- readme.txt
+++ readme.txt
@@ -1,22 +1,22 @@
 === Access Keys ===
 Contributors: Aaron Harun
 Donate link: http://anthologyoi.com/about/donate/
-Tags: accessibility, access keys, 
+Tags: accessibility, access keys,
 Requires at least: 2.1
-Tested up to: 2.3
-Stable tag: 0.6
+Tested up to: 2.5
+Stable tag: 1.0
 
-This plugin allows you to add Access Keys to Category and Page navigation menus to make your website far more accessible.
+Add Access Keys to Category and Page navigation menus to make your website far more accessible.
 
 == Description ==
-The WordPress Access Keys Plugin allows you to add access keys to pages and categories to improve accessibility of your WordPress website. This works with any page or category list that is called with wp_list pages and wp_list_categories respectively. (Translation: If your Wordpress theme works in any Wordpress version after 2.1 then this plugin should work for you.)
+The WordPress Access Keys Plugin allows you to add access keys to pages and categories to improve accessibility of your WordPress website. This works with any page or category list that is called with wp_list pages and wp_list_categories respectively.
 
-It is simple to use this this plugin. On the Admin panel it lists pages and categories with a current access key, and in a separate list it lists the pages and categories without access keys. Just add an access key just type an access key in a text box and then click the save button. The categories or posts will now move to the top list.
+It is simple to use this this plugin. On the Admin panel it lists pages and categories with a current access key, and in a separate list it lists the pages and categories without access keys. Just add an access key just type a number or letter in a text box and click the save button. The categories or posts will then move to the top list, and you will feel great about be kind to your users.
 
 == Installation ==
 1. Download the zip file, unzip it and upload to your wp-content/plugins folder
 1. Upload to your plugin directory.
 1. Activate.
 1. Go to the Admin Menu under Manage named Access Keys.
-1. Read the instructions.
+1. Read the short instructions.
 
